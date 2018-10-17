16e1fbfac282eba460524a29e0e431f69b1180ab
ZoranBajcer <zoran.bajcer@gmail.com>
2018-10-18 00:04:18 +0200

    Initial commit

---
+++ app/src/main/java/com/news/android/newsapp/About.java
@@ -0,0 +1,61 @@
+package com.news.android.newsapp;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+
+public class About extends AppCompatActivity {
+
+    TextView myTitleTextView;
+    TextView myDescTextView;
+    ImageView myImageView;
+    private String photo;
+
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //getting extras from main activity
+         String title = getIntent().getExtras().getString("title");
+         String description = getIntent().getExtras().getString("description");
+         photo = getIntent().getExtras().getString("photo");
+
+        myTitleTextView = (TextView)findViewById(R.id.naslov);
+        myDescTextView = (TextView)findViewById(R.id.opis);
+        myImageView = (ImageView)findViewById(R.id.imageView);
+
+        //setting title
+        myTitleTextView.setText(title);
+
+        //setting description
+        if(description.equals("null")){
+            myDescTextView.setText("Description will be added soon...");
+        }else { myDescTextView.setText(description); }
+
+        //setting photo
+        if(photo.equals("null")){
+            myImageView.setImageDrawable(getDrawable(R.drawable.goat));
+        }
+        else {
+            Picasso.get().load(photo).into(myImageView);
+        }
+
+        }
+
+
+    }
+
+
+
+
---
+++ app/src/main/java/com/news/android/newsapp/MainActivity.java
@@ -0,0 +1,144 @@
+package com.news.android.newsapp;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    int index;
+    private String aboutTitle;
+    private String aboutDescription;
+    private String aboutPhoto;
+    private String header;
+    private String TAG = MainActivity.class.getSimpleName();
+    private ListView lv;
+    JSONObject byIndex;
+    JSONArray article;
+    ArrayList<HashMap<String, String>> titleList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        titleList = new ArrayList<>();
+        lv = (ListView) findViewById(R.id.list);
+
+        new GetTitles().execute();
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                //position of selected JSON obj
+                index = position;
+
+                try {
+                    //getting JSON objects by index
+                    byIndex = article.getJSONObject(index);
+                    aboutTitle = byIndex.getString("title");
+                    aboutDescription = byIndex.getString("description");
+                    aboutPhoto = byIndex.getString("urlToImage");
+                }
+                catch (JSONException e){
+                    e.printStackTrace();
+                }
+                //sending data to About.class
+                Intent in = new Intent(MainActivity.this, About.class);
+                in.putExtra("title", aboutTitle);
+                in.putExtra("description", aboutDescription);
+                in.putExtra("photo", aboutPhoto);
+                startActivity(in);
+            }
+        });
+}
+    private class GetTitles extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        //Loading app before you say cookie
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this,"Say cookie.",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        //doing some progress in background
+        protected Void doInBackground(Void... arg0) {
+            URLconnection urlConn = new URLconnection();
+            // Making a request to url and getting response
+            String url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e402c76fc8584a1c81849179f1277a74";
+            //.........connection.........
+            String jsonStr = urlConn.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    article = jsonObj.getJSONArray("articles");
+
+                    // looping through articles
+                    for (int i = 0; i < article.length(); i++) {
+
+                        JSONObject c = article.getJSONObject(i);
+                        header = c.getString("title");
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> news = new HashMap<>();
+                        // adding each child node to HashMap key => value
+                        news.put("naslov", header);
+
+                        // adding titles to titleList
+                        titleList.add(news);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            ListAdapter adapter = new SimpleAdapter(MainActivity.this, titleList, R.layout.list_item, new String[]{ "naslov"}, new int[]{R.id.title});
+            lv.setAdapter(adapter);
+        }
+}
+
+}
